76aa650bd0cb4ecf9ebcabb7e4e30dd97bbf047e
Daniel Zaleta <zaletadaniel@gmail.com>
2022-04-14 13:03:33 -0500

    Tailwind ui, main layout and common components implemented

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  content: ['./src/**/*{html,js,jsx}'],
+  content: ["./src/**/*{html,js,jsx,ts,tsx}","./public/index.html"],
   theme: {
     colors: {
       ...colors,
