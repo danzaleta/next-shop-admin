c7acfa543dd798c34f1fc96753c50ea7cdbe6ba0
Daniel Zaleta <zaletadaniel@gmail.com>
2022-04-21 17:17:40 -0500

    Getting products from API

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useFetch = (endPoint) => {
+    const [data, setData] = useState([]);
+
+    async function fetchData() {
+        const response = await axios.get(endPoint);
+        setData(response.data);
+    }
+
+    useEffect(() => {
+        try {
+            fetchData();1
+        } catch (error) {
+            alert(error);
+        }
+    },[]);
+
+    return data;
+};
+
+export default useFetch;
--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -9,7 +9,16 @@ const people = [
     },
 ];
 
+import useFetch from "@hooks/useFetch";
+import endPoints from "@services/api";
+
 export default function Dashboard() {
+    const PRODUCT_LIMIT = 5;
+    const PRODUCT_OFFSET = 5;
+
+    const products = useFetch(endPoints.products.getProducts(PRODUCT_LIMIT, PRODUCT_OFFSET));
+    console.log(products);
+
     return (
         <>
             <div className="flex flex-col">
@@ -23,43 +32,49 @@ export default function Dashboard() {
                                             Name
                                         </th>
                                         <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                            Title
+                                            Category
                                         </th>
                                         <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                            Status
+                                            Price
                                         </th>
                                         <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                            Role
+                                            ID
                                         </th>
                                         <th scope="col" className="relative px-6 py-3">
                                             <span className="sr-only">Edit</span>
                                         </th>
+                                        <th scope="col" className="relative px-6 py-3">
+                                            <span className="sr-only">Delete</span>
+                                        </th>
                                     </tr>
                                 </thead>
                                 <tbody className="bg-white divide-y divide-gray-200">
-                                    {people.map((person) => (
-                                        <tr key={person.email}>
+                                    {products.map((product) => (
+                                        <tr key={`Product-Item-${product.id}`}>
                                             <td className="px-6 py-4 whitespace-nowrap">
                                                 <div className="flex items-center">
                                                     <div className="flex-shrink-0 h-10 w-10">
-                                                        <img className="h-10 w-10 rounded-full" src={person.image} alt="" />
+                                                        <img className="h-10 w-10 rounded-full" src={product.images[0]} alt="" />
                                                     </div>
                                                     <div className="ml-4">
-                                                        <div className="text-sm font-medium text-gray-900">{person.name}</div>
-                                                        <div className="text-sm text-gray-500">{person.email}</div>
+                                                        <div className="text-sm font-medium text-gray-900">{product.title}</div>
                                                     </div>
                                                 </div>
                                             </td>
                                             <td className="px-6 py-4 whitespace-nowrap">
-                                                <div className="text-sm text-gray-900">{person.title}</div>
-                                                <div className="text-sm text-gray-500">{person.department}</div>
+                                                <div className="text-sm text-gray-900">{product.category.name}</div>
                                             </td>
                                             <td className="px-6 py-4 whitespace-nowrap">
-                                                <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">Active</span>
+                                                <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">${product.price}</span>
                                             </td>
-                                            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{person.role}</td>
+                                            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{product.id}</td>
                                             <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                            Edit
+                                                Edit
+                                                <a href="#" className="text-indigo-600 hover:text-indigo-900">
+                                                </a>
+                                            </td>
+                                            <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                                Delete
                                                 <a href="#" className="text-indigo-600 hover:text-indigo-900">
                                                 </a>
                                             </td>
