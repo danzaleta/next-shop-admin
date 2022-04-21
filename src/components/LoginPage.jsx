942775a8b2ac8041d986e716f9239a1c48d99ae2
Daniel Zaleta <zaletadaniel@gmail.com>
2022-04-21 11:49:57 -0500

    Setting  user info

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,4 +1,5 @@
 import { useRef } from 'react';
+import { useRouter } from 'next/router';
 import { LockClosedIcon } from '@heroicons/react/solid';
 import { useAuth } from '@hooks/useAuth';
 
@@ -6,6 +7,7 @@ export default function LoginPage() {
     const emailRef = useRef(null);
     const passwordRef = useRef(null);
     const auth = useAuth();
+    const router = useRouter();
 
     const submitHandler = (event) => {
         event.preventDefault();
@@ -15,6 +17,7 @@ export default function LoginPage() {
         auth.signIn(email, password)
         .then(()=>{
             console.log('Logged!');
+            router.push('/dashboard');
         })
         .catch((error)=>{
             alert(error);
