3e3cf80b1072e6c31a16ca38b2dc76e6dd054c93
Daniel Zaleta <danielzaleta@MacBook-Pro-de-Daniel.local>
2022-04-22 17:17:12 -0500

    Products pagination

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,13 +4,11 @@ import '@styles/tailwind.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
       <ProviderAuth>
         <MainLayout>
           <Component {...pageProps} />
         </MainLayout>
       </ProviderAuth>
-    </>
   );
 }
 
--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,100 +1,192 @@
-import useFetch from "@hooks/useFetch";
-import endPoints from "@services/api";
-import { Chart } from "@common/Chart";
+import { useState } from 'react';
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid';
+import useFetch from '@hooks/useFetch';
+import endPoints from '@services/api';
+import { Chart } from '@common/Chart';
 
 export default function Dashboard() {
-    const PRODUCT_LIMIT = 15;
-    const PRODUCT_OFFSET = 15;
+  const PRODUCT_LIMIT = 100;
+  const PRODUCT_OFFSET = 0;
 
-    const products = useFetch(endPoints.products.getProducts(PRODUCT_LIMIT, PRODUCT_OFFSET));
-    const categoryNames = products?.map((product) => product.category);
-    const categoryCount = categoryNames?.map((category) => category.name);
-    const countOcurrences = (arr)=> arr.reduce((acc, curr)=>((acc[curr] = ++acc[curr] || 1), acc),{});
+  const products = useFetch(endPoints.products.getProducts(PRODUCT_LIMIT, PRODUCT_OFFSET));
+  const categoryNames = products?.map((product) => product.category);
+  const categoryCount = categoryNames?.map((category) => category.name);
+  const countOcurrences = (arr) => arr.reduce((acc, curr) => ((acc[curr] = ++acc[curr] || 1), acc), {});
 
-    console.log(products);
-    console.log(categoryNames);
-    console.log(categoryCount);
-    console.log(countOcurrences(categoryCount));
+  const [pageIdx, setPageIdx] = useState(0);
 
-    const data = {
-        datasets: [
-            {
-                label: 'Categories',
-                data: countOcurrences(categoryCount),
-                borderWidth: 2,
-                backgroundColor: ['#9dbc11', '#2ccbbb', '#f08bab', '#f3ba2f', '#9f88f9']
-            },
-        ],
-    }
+  const initialElement = pageIdx * 10;
+  const lastElement = initialElement + 10;
+  const currentSelection = products.slice(initialElement, lastElement);
 
-    return (
-        <>
-            <Chart className="mb-8 mt-2" chartData={data} />
-            <div className="flex flex-col">
-                <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                    <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                        <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                            <table className="min-w-full divide-y divide-gray-200">
-                                <thead className="bg-gray-50">
-                                    <tr>
-                                        <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                            Name
-                                        </th>
-                                        <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                            Category
-                                        </th>
-                                        <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                            Price
-                                        </th>
-                                        <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                            ID
-                                        </th>
-                                        <th scope="col" className="relative px-6 py-3">
-                                            <span className="sr-only">Edit</span>
-                                        </th>
-                                        <th scope="col" className="relative px-6 py-3">
-                                            <span className="sr-only">Delete</span>
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody className="bg-white divide-y divide-gray-200">
-                                    {products.map((product) => (
-                                        <tr key={`Product-Item-${product.id}`}>
-                                            <td className="px-6 py-4 whitespace-nowrap">
-                                                <div className="flex items-center">
-                                                    <div className="flex-shrink-0 h-10 w-10">
-                                                        <img className="h-10 w-10 rounded-full" src={product.images[0]} alt="" />
-                                                    </div>
-                                                    <div className="ml-4">
-                                                        <div className="text-sm font-medium text-gray-900">{product.title}</div>
-                                                    </div>
-                                                </div>
-                                            </td>
-                                            <td className="px-6 py-4 whitespace-nowrap">
-                                                <div className="text-sm text-gray-900">{product.category.name}</div>
-                                            </td>
-                                            <td className="px-6 py-4 whitespace-nowrap">
-                                                <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">${product.price}</span>
-                                            </td>
-                                            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{product.id}</td>
-                                            <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                                Edit
-                                                <a href="#" className="text-indigo-600 hover:text-indigo-900">
-                                                </a>
-                                            </td>
-                                            <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                                Delete
-                                                <a href="#" className="text-indigo-600 hover:text-indigo-900">
-                                                </a>
-                                            </td>
-                                        </tr>
-                                    ))}
-                                </tbody>
-                            </table>
+  const clickHandler = (value) => {
+    setPageIdx(value);
+    console.log(value);
+  };
+
+  const chartInfo = {
+    datasets: [
+      {
+        label: 'Categories',
+        data: countOcurrences(categoryCount),
+        borderWidth: 2,
+        backgroundColor: ['#9dbc11', '#2ccbbb', '#f08bab', '#f3ba2f', '#9f88f9'],
+      },
+    ],
+  };
+
+  return (
+    <>
+      <Chart className="mb-8 mt-2" chartData={chartInfo} />
+      <div className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
+        <div className="flex-1 flex justify-between sm:hidden">
+          <button
+            onClick={() => {
+                if (pageIdx > 0) {
+                    clickHandler(pageIdx - 1)
+                }
+            }}
+            className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+          >
+            Previous
+          </button>
+          <button
+            onClick={() => {
+                if (pageIdx < 9) {
+                    clickHandler(pageIdx + 1)
+                }
+            }}
+            className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+          >
+            Next
+          </button>
+        </div>
+        <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
+          <div>
+            <p className="text-sm text-gray-700">
+              Showing <span className="font-medium">{initialElement + 1}</span> to <span className="font-medium">{lastElement}</span> of <span className="font-medium">{products.length}</span> results
+            </p>
+          </div>
+          <div>
+            <nav className="relative z-0 inline-flex rounded-md shadow-sm -space-x-px" aria-label="Pagination">
+              <button
+                onClick={() => {
+                    if (pageIdx > 0) {
+                        clickHandler(pageIdx - 1)
+                    }
+                }}
+                className="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+              >
+                <span className="sr-only">Previous</span>
+                <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
+              </button>
+              {/* Current: "z-10 bg-indigo-50 border-indigo-500 text-indigo-600", Default: "bg-white border-gray-300 text-gray-500 hover:bg-gray-50" */}
+              <button onClick={() => clickHandler(0)} className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium">
+                1
+              </button>
+              <button onClick={() => clickHandler(1)} className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium">
+                2
+              </button>
+              <button
+                onClick={() => clickHandler(2)}
+                className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 hidden md:inline-flex relative items-center px-4 py-2 border text-sm font-medium"
+              >
+                3
+              </button>
+              <span className="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700">...</span>
+              <button
+                onClick={() => clickHandler(7)}
+                className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 hidden md:inline-flex relative items-center px-4 py-2 border text-sm font-medium"
+              >
+                8
+              </button>
+              <button onClick={() => clickHandler(8)} className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium">
+                9
+              </button>
+              <button onClick={() => clickHandler(9)} className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium">
+                10
+              </button>
+              <button
+                onClick={() => {
+                    if (pageIdx < 9) {
+                        clickHandler(pageIdx + 1)
+                    }
+                }}
+                className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+              >
+                <span className="sr-only">Next</span>
+                <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
+              </button>
+            </nav>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col">
+        <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+              <table className="min-w-full divide-y divide-gray-200">
+                <thead className="bg-gray-50">
+                  <tr>
+                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                      ID
+                    </th>
+                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                      Name
+                    </th>
+                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                      Category
+                    </th>
+                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                      Price
+                    </th>
+
+                    <th scope="col" className="relative px-6 py-3">
+                      <span className="sr-only">Edit</span>
+                    </th>
+                    <th scope="col" className="relative px-6 py-3">
+                      <span className="sr-only">Delete</span>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="bg-white divide-y divide-gray-200">
+                  {currentSelection.map((product) => (
+                    <tr key={`Product-Item-${product.id}`}>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{product.id}</td>
+                      <td className="px-6 py-4 whitespace-nowrap">
+                        <div className="flex items-center">
+                          <div className="flex-shrink-0 h-10 w-10">
+                            <img className="h-10 w-10 rounded-full" src={product.images[0]} alt="" />
+                          </div>
+                          <div className="ml-4">
+                            <div className="text-sm font-medium text-gray-900">{product.title}</div>
+                          </div>
                         </div>
-                    </div>
-                </div>
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap">
+                        <div className="text-sm text-gray-900">{product.category.name}</div>
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap">
+                        <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">$ {product.price}</span>
+                      </td>
+
+                      <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                        Edit
+                        <a href="#" className="text-indigo-600 hover:text-indigo-900"></a>
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                        Delete
+                        <a href="#" className="text-indigo-600 hover:text-indigo-900"></a>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
             </div>
-        </>
-    );
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
