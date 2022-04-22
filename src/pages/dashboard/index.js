c9f22fbd026a23f4846e1607d85386d99d98af52
Daniel Zaleta <danielzaleta@MacBook-Pro-de-Daniel.local>
2022-04-22 17:21:18 -0500

    Pagination bar on bottom

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -187,6 +187,90 @@ export default function Dashboard() {
           </div>
         </div>
       </div>
+
+      <div className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
+        <div className="flex-1 flex justify-between sm:hidden">
+          <button
+            onClick={() => {
+                if (pageIdx > 0) {
+                    clickHandler(pageIdx - 1)
+                }
+            }}
+            className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+          >
+            Previous
+          </button>
+          <button
+            onClick={() => {
+                if (pageIdx < 9) {
+                    clickHandler(pageIdx + 1)
+                }
+            }}
+            className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+          >
+            Next
+          </button>
+        </div>
+        <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
+          <div>
+            <p className="text-sm text-gray-700">
+              Showing <span className="font-medium">{initialElement + 1}</span> to <span className="font-medium">{lastElement}</span> of <span className="font-medium">{products.length}</span> results
+            </p>
+          </div>
+          <div>
+            <nav className="relative z-0 inline-flex rounded-md shadow-sm -space-x-px" aria-label="Pagination">
+              <button
+                onClick={() => {
+                    if (pageIdx > 0) {
+                        clickHandler(pageIdx - 1)
+                    }
+                }}
+                className="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+              >
+                <span className="sr-only">Previous</span>
+                <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
+              </button>
+              {/* Current: "z-10 bg-indigo-50 border-indigo-500 text-indigo-600", Default: "bg-white border-gray-300 text-gray-500 hover:bg-gray-50" */}
+              <button onClick={() => clickHandler(0)} className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium">
+                1
+              </button>
+              <button onClick={() => clickHandler(1)} className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium">
+                2
+              </button>
+              <button
+                onClick={() => clickHandler(2)}
+                className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 hidden md:inline-flex relative items-center px-4 py-2 border text-sm font-medium"
+              >
+                3
+              </button>
+              <span className="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700">...</span>
+              <button
+                onClick={() => clickHandler(7)}
+                className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 hidden md:inline-flex relative items-center px-4 py-2 border text-sm font-medium"
+              >
+                8
+              </button>
+              <button onClick={() => clickHandler(8)} className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium">
+                9
+              </button>
+              <button onClick={() => clickHandler(9)} className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium">
+                10
+              </button>
+              <button
+                onClick={() => {
+                    if (pageIdx < 9) {
+                        clickHandler(pageIdx + 1)
+                    }
+                }}
+                className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+              >
+                <span className="sr-only">Next</span>
+                <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
+              </button>
+            </nav>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
